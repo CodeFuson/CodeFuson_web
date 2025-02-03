c1534b520067f97e7b8d2a53cebcaa7fc7f73b12
JLoloCkik <jankailorinc1@gmail.com>
2025-02-03 23:06:41 +0100

    realtime update

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -2,11 +2,12 @@ import React from 'react';
 import '../css/home_page.css';
 
 const HomePage = () => {
-  return (
+    return (
     <div className="chat-container">
-      <input type="text" className="chat-input" placeholder="Type your message..." />
+        <input type="text" className="chat-input" placeholder="Type your message..."/>
     </div>
-  );
+)
+    ;
 };
 
 export default HomePage;
